e8ce1d2441ccce07466c3db5764fbd8367ff928d
damonponder <damonponder@MacBook-Pro.local>
2026-04-10 13:00:21 -0500

    Converted Header component to reusable prop-based structure with navigation controls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
 
+const headerData = [
+  { title: "This is a Title" },
+  { title: "This is a Second Title" },
+  { title: "This is a Third Title" }
+];
+
 const App = () => {
   return (
     <div className='container'>
-     <Header />
+     <Header titles={headerData} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,27 @@
 import { useState } from "react";
 
-let header = [
-  { title: "This is a Title" },
-  { title: "This is a Second Title" },
-  { title: "This is a Third Title" }
-];
 
-const Header = () => {
+
+const Header = ({ titles}) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <div>
       <header style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
         <h1 style={{ color: "black" }}>
-          {header[currentIndex].title}
+          {titles[currentIndex].title}
         </h1>
       </header>
     <div>
          <p style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
-          {currentIndex + 1} of {header.length}
+          {currentIndex + 1} of {titles.length}
         </p>
     </div>
     <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
-        <button onClick={() => setCurrentIndex(prev => (prev - 1 + header.length) % header.length)}>
+        <button onClick={() => setCurrentIndex(prev => (prev - 1 + titles.length) % titles.length)}>
       Previous
     </button>
-      <button style={{ marginLeft: "20px" }} onClick={() => setCurrentIndex(prev => (prev + 1) % header.length)}>
+      <button style={{ marginLeft: "20px" }} onClick={() => setCurrentIndex(prev => (prev + 1) % titles.length)}>
       Next
     </button>
     </div>
