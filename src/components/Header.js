cddc12bb3e9a959f3f63498d3983f932bbadfaa9
damonponder <damonponder@MacBook-Pro.local>
2026-04-10 11:41:17 -0500

    Built React title navigator with next/previous controls and index tracking

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,15 +11,24 @@ const Header = () => {
 
   return (
     <div>
-      <header>
+      <header style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
         <h1 style={{ color: "black" }}>
           {header[currentIndex].title}
         </h1>
       </header>
-
-      <button onClick={() => setCurrentIndex(prev => (prev + 1) % header.length)}>
-        Change Title 
-      </button>
+    <div>
+         <p style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+          {currentIndex + 1} of {header.length}
+        </p>
+    </div>
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+        <button onClick={() => setCurrentIndex(prev => (prev - 1 + header.length) % header.length)}>
+      Previous
+    </button>
+      <button style={{ marginLeft: "20px" }} onClick={() => setCurrentIndex(prev => (prev + 1) % header.length)}>
+      Next
+    </button>
+    </div>
     </div>
   );
 };
